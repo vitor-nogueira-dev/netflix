d97fba832509365108f040c8147131ca6eacb32e
Vitor Nogueira <vslnogueira96@gmail.com>
2022-12-10 15:51:54 -0300

    Criando estilização do chosen (filme escolhido)

--- src/components/FeaturedMovie.jsx
+++ src/components/FeaturedMovie.jsx
@@ -2,9 +2,36 @@ import React from 'react'
 import './FeaturedMovie.css'
 
 const FeaturedMovie = ({ item }) => {
+  const firstDate = new Date(item.first_air_date);
+  let genres = [];
+  for (let index  in item.genres) {
+    genres.push(item.genres[index].name)
+  }
+
   return (
-    <section className="featured">
-      <div >{item.original_name}</div>
+    <section className="featured" 
+      style={{
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+      }}
+    >
+      <div className="featured--vertical">
+        <div className="featured--horizontal">
+          <div className="featured--name">{item.original_name}</div>
+          <div className="featured--info">
+            <div className="featured--points">{(item.vote_average.toFixed(1))} pontos</div>
+            <div className="featured--year">{firstDate.getFullYear()}</div>
+            <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''} </div>
+          </div>
+          <div className="featured--description">{item.overview}</div>
+          <div className="featured--buttons">
+            <a href={`/watch/${item.id}`} className="featured--watchbutton"> ▶ Assistir</a>
+            <a href={`/list/add/${item.id}`} className="featured--mylistbutton"> + Minha Lista</a>
+          </div>
+          <div className="featured--genres"><strong>Gêneros: {genres.join(', ')}</strong></div>
+        </div> 
+      </div>
     </section>
   )
 }
